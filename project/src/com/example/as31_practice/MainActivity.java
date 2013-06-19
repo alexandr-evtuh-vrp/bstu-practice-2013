97f45a6a05e4bcb347f0831bd1e1fa4c5e6846c1
bstuAS31Alexandr <sevendiss@gmail.com>
2013-06-19 22:17:32 +0300

    Добавлен обработчик нажатия кнопки.

--- project/src/com/example/as31_practice/MainActivity.java
+++ project/src/com/example/as31_practice/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.as31_practice;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+	
+	Button btnStart, btnSettings;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		btnStart = (Button) findViewById(R.id.btnStart);
+		btnSettings = (Button) findViewById(R.id.btnSettings);
+		
+		btnStart.setOnClickListener(this);
+		btnSettings.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +28,21 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.btnStart:
+			// code...
+			
+		case R.id.btnSettings:
+			// code...
+		default:
+			break;
+		}
+		
 	}
 
 }
